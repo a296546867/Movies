8b72acdcec8207233ed976c286e385881f24eadd
chenyp <296546867@qq.com>
2016-12-29 17:28:57 +0800

    添加图书

--- Movies/src/hadesky/controller/ControllerServlet.java
+++ Movies/src/hadesky/controller/ControllerServlet.java
@@ -1,59 +1,152 @@
 package hadesky.controller;
 
+import hadesky.domain.Book;
 import hadesky.domain.Category;
 import hadesky.service.Business;
 import hadesky.service.impl.BusinessImpl;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.lang.reflect.InvocationTargetException;
 import java.util.List;
+import java.util.UUID;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileUpload;
+import org.apache.commons.fileupload.FileUploadException;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import org.apache.commons.io.FilenameUtils;
+
+import com.sun.org.apache.commons.beanutils.BeanUtils;
+import com.sun.org.apache.xerces.internal.impl.dv.ValidatedInfo;
+
 import utli.FillBeanUtil;
 
 public class ControllerServlet extends HttpServlet {
 	private Business s = new BusinessImpl();
+
 	public void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
 		String opString = request.getParameter("op");
 		if ("addCategory".equals(opString)) {
-			addCategory(request,response);
-		}else if ("showAllCategories".equals(opString)) {
-			showAllCategories(request,response);
-		}
-		
-		
-	}
-	//查询分类
-	private void showAllCategories(HttpServletRequest request,
-			HttpServletResponse response) {
+			addCategory(request, response);
+		} else if ("showAllCategories".equals(opString)) {
+			showAllCategories(request, response);
+		} else if ("showAddBookUI".equals(opString)) {
+			showAddBookUI(request, response);
+		}else if ("addBook".equals(opString)) {
 			try {
-				List<Category> list = s.findAllCategories();
-				request.setAttribute("sc", list);
-				request.getRequestDispatcher("/Admin/listCategory.jsp").forward(request, response);
-			} catch (ServletException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+				addBook(request,response);
+			} catch (Exception e) {
+				throw new RuntimeException(e);
 			}
-			
+		}
+
 	}
 
-	//添加分类
-	private void addCategory(HttpServletRequest request, HttpServletResponse response) {
+	//添加图书
+	private void addBook(HttpServletRequest request,
+			HttpServletResponse response) throws Exception {
+
+		boolean isMultpart = ServletFileUpload.isMultipartContent(request);
+		if (!isMultpart) {
+			request.setAttribute("msg", "保存失败");
+			request.getRequestDispatcher("/Admin/message.jsp").forward(request, response);
+		return;
+		}
+		
+		ServletFileUpload fileUpload = new ServletFileUpload(new DiskFileItemFactory());
+		List<FileItem> items = fileUpload.parseRequest(request);
+		
+		Book book = new Book();
+		for (FileItem item : items) {
+			if (item.isFormField()) {
+				String nameString = item.getFieldName();
+				String vauleString = item.getString(request.getCharacterEncoding());
+				BeanUtils.setProperty(book, nameString, vauleString);
+			}else{
+				//文件上传
+				String fileName = item.getName();
+				if(fileName!=null&&!fileName.trim().equals("")){
+					//文件名
+					fileName = UUID.randomUUID().toString()+"."+FilenameUtils.getExtension(fileName);
+					//文件路径
+					String storeDirectory = getServletContext().getRealPath("/images");
+					String path = makeDirs(storeDirectory, fileName);//   /dir1/dir2
+					
+					book.setPath(path);
+					book.setPhotoFileName(fileName);
+					
+					//写入文件
+					item.write(new File(storeDirectory+path+"/"+fileName));
+				}
+			}
+		}
+		//保存
+		s.addBook(book);
+		request.setAttribute("msg","添加成功");
+		request.getRequestDispatcher("/Admin/message.jsp").forward(request, response);
+	}
+
+	//创建文件保存路径
+	private String makeDirs(String storeDirectory, String fileName) {
+		int hashcode = fileName.hashCode();
+		int dir1 = hashcode&0xf;
+		int dir2 = (hashcode&0xf0)>>4;
+		
+		String newPath = "/"+dir1+"/"+dir2;
+		File file = new File(storeDirectory, newPath);
+		if(!file.exists()){
+			file.mkdirs();
+		}
+		return newPath;
+	}
+
+	// 添加图书，先查出分类信息
+	private void showAddBookUI(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		List<Category> list = s.findAllCategories();
+		request.setAttribute("c", list);
+		request.getRequestDispatcher("/Admin/addBooks.jsp").forward(request,
+				response);
+	}
+
+	// 查询分类
+	private void showAllCategories(HttpServletRequest request,
+			HttpServletResponse response) {
+		try {
+			List<Category> list = s.findAllCategories();
+			request.setAttribute("sc", list);
+			request.getRequestDispatcher("/Admin/listCategory.jsp").forward(
+					request, response);
+		} catch (ServletException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	// 添加分类
+	private void addCategory(HttpServletRequest request,
+			HttpServletResponse response) {
 		Category category = FillBeanUtil.fillBean(request, Category.class);
 		try {
 			s.addCategory(category);
 			request.setAttribute("msg", "success");
-			request.getRequestDispatcher("/Admin/message.jsp").forward(request, response);
-			
+			request.getRequestDispatcher("/Admin/message.jsp").forward(request,
+					response);
+
 		} catch (ServletException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
